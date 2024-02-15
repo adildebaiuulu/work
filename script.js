35d3e689fc7f23c8f0176d5ba66f53d05a14e03a
Arsen <arich.sett@gmail.com>
2024-02-16 01:15:12 +0300

    Кутман таң

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ alert('Arsen')
 
 document.body.innerHTML = "Кутман таң" 
 
-Math.ceil(6.400)
-Math.floor(181.900)
-Math.round(39.320)
-Math.round(18,6+195,3)
-Math.trunc(0+10)
+console.log(Math.ceil(6.400))
+console.log(Math.floor(181.900))
+console.log(Math.round(39.320))
+console.log(Math.round(18.6+195.3))
+console.log(Math.random() * 10)
 
 
