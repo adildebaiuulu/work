5dcb994c4e067dad19a0f6c0f0256f43133a9ba9
Arsen <arich.sett@gmail.com>
2024-02-16 00:10:37 +0300

    Кутман таң

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert('Кутман таң')
+alert('Arsen')
+
+document.body.innerHTML = "Кутман таң" 
+
+Math.ceil(6.400)
+Math.floor(181.900)
+Math.round(39.320)
+Math.round(18,6+195,3)
+Math.trunc(0+10)
+
+
